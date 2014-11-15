5fb036fb57a79c733d252c28c10ee581f37c380c
Justin Conover <justin.conover@gmail.com>
2014-11-15 11:37:59 -0800

    Initial commit.

---
+++ manifests/config.pp
@@ -0,0 +1,13 @@
+class ssh::config {
+  include ssh::params
+
+  file { "/etc/ssh/sshd_config":
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => 0440,
+    source  => "puppet:///modules/ssh/sshd_config",
+    require => Class["ssh::install"],
+    notify  => Class["ssh::service"],
+  }
+}
